e430230a037dfbf8b57903301deafc1b80ca85f4
ViniciusMargotti <viniciusmargotti@hotmail.com>
2019-11-02 23:45:56 -0300

    Correção classe cliente

--- equipamentos-apirest/equipamentos-apirest/src/main/java/com/equipamentos/apirest/models/Cliente.java
+++ equipamentos-apirest/equipamentos-apirest/src/main/java/com/equipamentos/apirest/models/Cliente.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 
 
 @Entity
-@Table(name="EQUIPAMENTO")
+@Table(name="CLIENTE")
 public class Cliente implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
